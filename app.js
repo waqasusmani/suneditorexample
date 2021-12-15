ffbde0cbfb2839ce6b0928556e7a61291bb0232e
waqasusmani <waqas-usmani@hotmail.com>
2021-12-16 00:31:21 +0500

    Editor and extracted data

---
+++ app.js
@@ -0,0 +1,34 @@
+/**
+* ID : 'suneditor_sample'
+* ClassName : 'sun-eidtor'
+*/
+// ID or DOM object
+const editor = SUNEDITOR.create((document.getElementById('sample') || 'sample'),{
+    // All of the plugins are loaded in the "window.SUNEDITOR" object in dist/suneditor.min.js file
+    // Insert options
+    // Language global object (default: en)
+    lang: SUNEDITOR_LANG['en'],
+    buttonList: [
+        ['undo', 'redo'],
+        ['font', 'fontSize', 'formatBlock'],
+        ['paragraphStyle', 'blockquote'],
+        ['bold', 'underline', 'italic', 'strike', 'subscript', 'superscript'],
+        ['fontColor', 'hiliteColor', 'textStyle'],
+        ['removeFormat'],
+        '/', // Line break
+        ['outdent', 'indent'],
+        ['align', 'horizontalRule', 'list', 'lineHeight'],
+        ['table', 'link', 'image', 'video', 'audio' /** ,'math' */], // You must add the 'katex' library at options to use the 'math' plugin.
+        /** ['imageGallery'] */ // You must add the "imageGalleryUrl".
+        ['fullScreen', 'showBlocks', 'codeView'],
+        ['preview', 'print'],
+        ['save', 'template']
+    ]
+});
+
+function extractData() {
+    const editorData=editor.getContents()
+    console.log(typeof(editorData))
+    document.getElementById("contents-div").innerHTML=editorData
+}
+
